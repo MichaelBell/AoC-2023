e4bc050ae7022cb0cd0b4db00fddb6f493800430
Mike Bell <mdb036@gmail.com>
2023-12-18 22:34:33 +0000

    Day 18

--- aoc.h
+++ aoc.h
@@ -4,6 +4,14 @@
 #include <stdint.h>
 #include "aoc_plat.h"
 
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
 const char* read_num(const char* ptr, int* num) {
     *num = 0;
     bool negate = *ptr == '-';
